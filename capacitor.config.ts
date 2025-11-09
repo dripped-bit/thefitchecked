6aa333820bdf6ef5e64e2dddc20017290acc7682
dripped-bit <dripped@thefitchecked.com>
2025-11-08 20:32:33 -0600

    Update project settings and dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.thefitchecked.app',
+  appName: 'TheFitChecked',
+  webDir: 'dist'
+};
+
+export default config;
